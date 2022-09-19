71def8473152a3b1ee9732ee5c0a2165d4daf8b6
ST2712 <111332821+ST2712@users.noreply.github.com>
2022-09-18 22:56:28 -0500

    Se arreglan errores y se deja al día, borrar empleados para que la base de datos actualice el id de la empresa en dashboardEmpleados

--- desaInt/src/main/resources/static/js/util.js
+++ desaInt/src/main/resources/static/js/util.js
@@ -1,3 +1,11 @@
 function alertaCrearEmpleados(){
     alert("Ya estas es la pagina para crear empleados")
 }
+
+function alertaListarEmpleados(){
+    alert("Ya estas es la pagina para listar a los empleados")
+}
+
+function alertaListarEmpresas(){
+    alert("Ya estas en la pagina para listar a las empresas")
+}
