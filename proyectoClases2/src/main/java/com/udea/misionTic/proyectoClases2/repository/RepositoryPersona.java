f4dcef2e522812ec8c0174dfbe469aef93c6633a
ST2712 <111332821+ST2712@users.noreply.github.com>
2022-09-24 18:44:28 -0500

    Borrado para poder hacer merge

--- proyectoClases2/src/main/java/com/udea/misionTic/proyectoClases2/repository/RepositoryPersona.java
+++
@@ -1,6 +0,0 @@
-package com.udea.misionTic.proyectoClases2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepositoryPersona extends JpaRepository<EntityPersona, Long> {
-}
--- proyectoClases2/src/main/java/com/udea/misionTic/proyectoClases2/util/EnumRol.java
+++
@@ -1,6 +0,0 @@
-package com.udea.misionTic.proyectoClases2.util;
-
-public enum EnumRol {
-
-    ADMIN, USER, VISITANTE;
-}
